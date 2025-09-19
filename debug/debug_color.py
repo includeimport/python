511b312989fbfcafd9f2a6fddec76152db0f25e8
includeimport <200589118+includeimport@users.noreply.github.com>
2025-09-19 13:53:03 +0900

    feat: Add frame info to Debug functions
    Change default functions to print function name, file name, line number using frame info when called.

--- debug/debug_color.py
+++ debug/debug_color.py
@@ -1,21 +1,44 @@
 from enum import Enum
+import inspect
+import os
+from types import FrameType
 
 class Text_Color(Enum):
     RESET = "\033[0m"
     RED = "\033[31m"
     GRENN = "\033[32m"
     YELLOW = "\033[33m"
+    CYAN = "\033[36m"
 
 class Debug:
-    def fail(text):
-        print(f"{Text_Color.RED.value}[Fail]: {text}{Text_Color.RESET.value}")
 
-    def success(text):
-        print(f"{Text_Color.GRENN.value}[Success]: {text}{Text_Color.RESET.value}")
+    def get_frame_info(self, frame: FrameType):
+        """
+        호출한 함수의 이름, 파일명, 줄 번호 반환
+        """
+        func_name = frame.f_code.co_name
+        full_path = frame.f_code.co_filename
+        file_name = os.path.basename(full_path)
+        line_no = frame.f_back.f_lineno
 
-    def log(text):
-        print(f"{Text_Color.YELLOW.value}[Log]: {text}{Text_Color.RESET.value}")
+        return (func_name, file_name, line_no)
 
-#print(f"{Text_Color.GRENN.value}Test_Green{Text_Color.RESET.value}")
+    def print_with_color(self, color: Text_Color, text):
+        print(f"{color.value}{text}{Text_Color.RESET.value}")
+        
+    def simple_check(self, text):
+        self.print_with_color(Text_Color.CYAN, text)
 
-Debug.success("testing")
+    def check(self, text):
+        func_name, file_name, line_no = self.get_frame_info(inspect.currentframe())
+        edit_text = (
+                f"[Check]: {text}"
+                f" (File \"{file_name}\", line {line_no} in \"{func_name}\")"
+
+            )
+        self.print_with_color(Text_Color.CYAN, edit_text)
+
+obj = Debug()
+obj.simple_check("simple test")
+obj.check("deep test")
+
